b9d5eb09c37d1c3b5bac362b4efbb3a4123e914f
GangulyAnubhav <anubhavganguly.icloud@gmail.com>
2026-08-21 10:49:18 +0530

    Pushing changes regarding contextAPI learning.

--- MiniContext/src/App.jsx
+++ MiniContext/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
 import UserContextProvider from './context/userContextProvider'
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <UserContextProvider>
       <h1>Context API</h1>
+      <Login />
+      <Profile />
     </UserContextProvider>
   )
 }
---
+++ MiniContext/src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React, {useContext} from 'react'
+import UserContext from '../context/userContext'
+
+function Profile() {
+
+    const {user} = useContext(UserContext)
+  
+    if(!user) {
+        return <h2>Please login</h2>
+    }
+    return (
+    <div>
+        <h2>Welcome {user.username}</h2>
+    </div>
+  )
+}
+
+export default Profile
